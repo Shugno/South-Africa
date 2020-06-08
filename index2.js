4a35501d42a0886225f5b459e6c5a9f9c99ccd3a
Shugno <65904115+Shugno@users.noreply.github.com>
2020-06-08 15:39:40 +0200

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,39 @@
+var Timer = 0;
+var text1 = document.getElementById("forimg1");
+var text2 = document.getElementById("forimg2");
+var text3 = document.getElementById("forimg3");
+var text4 = document.getElementById("forimg4");
+
+setInterval(function(){
+	Timer++;
+
+	if (Timer == 6)
+	{
+		document.getElementById('ImgBackground').src = "img/Dance2.jpg";
+		text1.style.visibility = "hidden";
+		text2.style.visibility = "visible";
+		document.getElementById("changetitle").innerHTML = "The Dzilla of the Ndebele nation";
+	}
+	else if (Timer == 12)
+	{
+		document.getElementById('ImgBackground').src = "img/Dance3.jpg";
+		text2.style.visibility = "hidden";
+		text3.style.visibility = "visible";
+		document.getElementById("changetitle").innerHTML = "The Sokkie Dance of the afrikaners";
+	}
+	else if (Timer == 18)
+	{
+		document.getElementById('ImgBackground').src = "img/Dance4.jpg";
+		text3.style.visibility = "hidden";
+		text4.style.visibility = "visible";
+		document.getElementById("changetitle").innerHTML = "The cape minstrels of the cape malay people";
+	}
+	else if (Timer == 24)
+	{
+		document.getElementById('ImgBackground').src = "img/Dance.jpg";
+		text4.style.visibility = "hidden";
+		text1.style.visibility = "visible";
+		document.getElementById("changetitle").innerHTML = "The ingoma dance of the Zulu nation";
+		Timer = 0;	
+	}
+}, 1000);
---
+++ index4.js
@@ -0,0 +1,94 @@
+var imgDiv = document.getElementById("img1Text");
+
+var pos = 0;
+var Font = 25;
+var Timer = 0;
+
+var text1 = document.getElementById("forimg1");
+var text2 = document.getElementById("forimg2");
+var text3 = document.getElementById("forimg3");
+var text4 = document.getElementById("forimg4");
+
+var img = document.getElementById("roundImg");
+img.mouseIsOver = false;
+
+var text = document.getElementById("animText1");
+
+function testnew()
+{
+    var t = setInterval(test1, 10);
+    function test1 ()
+{
+     if (pos > 0 && img.mouseIsOver == false)
+     {
+         clearInterval(t);
+     }
+     if (pos >= 900)
+     {
+        clearInterval(t);
+     }
+     else
+     {
+         pos += 18;
+         if (pos >= 700)
+         {
+             text.style.visibility = "visible";
+         }
+         imgDiv.style.width = pos + "px";
+         img.mouseIsOver = true;
+     }
+}
+}
+
+function testback()
+{
+    t = setInterval(test2, 10);
+    function test2 ()
+    {
+        if (pos <= 0)
+        {
+            clearInterval(t);
+        }
+        else
+        {
+            if (pos <= 700)
+            {
+                Font -= 9;
+                text.style.left = Font + "px";
+                if (pos <= 700)
+                {
+                    text.style.visibility = "hidden";
+                    text.style.left = "0.2vw";
+                }
+            }
+            pos -= 18;
+            imgDiv.style.width = pos + "px";
+            img.mouseIsOver = false;
+        }
+    }
+}
+
+setInterval(function(){
+	Timer++;
+
+	if (Timer == 10)
+	{
+		document.getElementById('ImgBackground').src = "img/Tradition.jpg";
+		text1.style.visibility = "hidden";
+		text2.style.visibility = "visible";
+	}
+	else if (Timer == 22)
+	{
+		document.getElementById('ImgBackground').src = "img/africacool.png";
+		text2.style.visibility = "hidden";
+		text3.style.visibility = "visible";
+	}
+
+	else if (Timer == 34)
+	{
+		document.getElementById('ImgBackground').src = "img/finbild.jpg";
+		text4.style.visibility = "hidden";
+		text1.style.visibility = "visible";
+		Timer = 0;	
+	}
+}, 1000);
